76f0ffd6071c3c990dcf371cbd911956bd04b85e
siddhant209 <siddhant_jaiswal@applicatetechnology.com>
2021-10-18 16:20:44 +0530

    Added instances table-some error parsing response

--- controllers/describeVPCandInstances.js
+++ controllers/describeVPCandInstances.js
@@ -1,85 +1,99 @@
+const {
+  DescribeVpcsCommand,
+  DescribeInstancesCommand,
+} = require("@aws-sdk/client-ec2");
+const { ec2Client } = require("../libs/ec2Client");
 
-const {DescribeVpcsCommand }= require("@aws-sdk/client-ec2")
-const {ec2Client} =require('../libs/ec2Client')
-
-const describeVPCs = async () => {
-    const params = { };
-    try {
-      const data = await ec2Client.send(
-        new DescribeVpcsCommand(params)
-      );
-      //console.log("Success...here's ur VPCs: ", JSON.stringify(data.Vpcs));
-      return data;
-    } catch (err) {
-      //console.log("Oh no!!!.. Error", err);
-      return err;
-    }
-  };
-const describeVPCandInstances= async (req,res)=>{
-    const params = { };
-    try {
-        const response = await ec2Client.send(
-          new DescribeVpcsCommand(params)
-        );
-        const defaultResponse=[
+const getVPCs = async () => {
+  const params = {};
+  try {
+    const response = await ec2Client.send(new DescribeVpcsCommand(params));
+    const defaultResponse = {
+      Vpcs: [
+        {
+          CidrBlock: "10.0.0.0/24",
+          DhcpOptionsId: "dopt-00c5fd7a",
+          State: "available",
+          VpcId: "vpc-0b3a6af15277cb800",
+          OwnerId: "292242767239",
+          InstanceTenancy: "default",
+          CidrBlockAssociationSet: [
             {
-               "CidrBlock":"10.0.0.0/24",
-               "DhcpOptionsId":"dopt-00c5fd7a",
-               "State":"available",
-               "VpcId":"vpc-0b3a6af15277cb800",
-               "OwnerId":"292242767239",
-               "InstanceTenancy":"default",
-               "CidrBlockAssociationSet":[
-                  {
-                     "AssociationId":"vpc-cidr-assoc-0e13f4b8f6ac06463",
-                     "CidrBlock":"10.0.0.0/24",
-                     "CidrBlockState":{
-                        "State":"associated"
-                     }
-                  }
-               ],
-               "IsDefault":false,
-               "Tags":[
-                  {
-                     "Key":"Name",
-                     "Value":"demovpc"
-                  }
-               ]
+              AssociationId: "vpc-cidr-assoc-0e13f4b8f6ac06463",
+              CidrBlock: "10.0.0.0/24",
+              CidrBlockState: {
+                State: "associated",
+              },
             },
+          ],
+          IsDefault: false,
+          Tags: [
             {
-               "CidrBlock":"172.31.0.0/16",
-               "DhcpOptionsId":"dopt-00c5fd7a",
-               "State":"available",
-               "VpcId":"vpc-067ce37b",
-               "OwnerId":"292242767239",
-               "InstanceTenancy":"default",
-               "CidrBlockAssociationSet":[
-                  {
-                     "AssociationId":"vpc-cidr-assoc-04d1b96b",
-                     "CidrBlock":"172.31.0.0/16",
-                     "CidrBlockState":{
-                        "State":"associated"
-                     }
-                  }
-               ],
-               "IsDefault":true
-            }
-         ]
-        //console.log("Success...here's ur VPCs: ", JSON.stringify(data.Vpcs));
-        //res.render('index.ejs', {data.Vpcs});
-        const data= response.Vpcs.map(vpc=>{
-            const vpcName= vpc.Tags?.[0].Value || "-"
-            return {name:vpcName, id: vpc.VpcId, cidr: vpc.CidrBlock}
-        })
-        res.render('index.ejs', {data});
-      } catch (err) {
-        console.log("Oh no!!!.. Error %j",err );
-        
-        console.log(err.type,err.message)
-        //res.send(err.message)
-        res.render('index.ejs', {data:[], error:err.message});
-      }
+              Key: "Name",
+              Value: "demovpc",
+            },
+          ],
+        },
+        {
+          CidrBlock: "172.31.0.0/16",
+          DhcpOptionsId: "dopt-00c5fd7a",
+          State: "available",
+          VpcId: "vpc-067ce37b",
+          OwnerId: "292242767239",
+          InstanceTenancy: "default",
+          CidrBlockAssociationSet: [
+            {
+              AssociationId: "vpc-cidr-assoc-04d1b96b",
+              CidrBlock: "172.31.0.0/16",
+              CidrBlockState: {
+                State: "associated",
+              },
+            },
+          ],
+          IsDefault: true,
+        },
+      ],
+    };
 
-}
+    const data = defaultResponse.Vpcs.map((vpc) => {
+      const vpcName = vpc.Tags?.[0].Value || "-";
+      return { name: vpcName, id: vpc.VpcId, cidr: vpc.CidrBlock };
+    });
+    return { data };
+  } catch (err) {
+    console.log("Oh no!!!.. Error from vpcs %j");
+    return { data: [], error: err.message };
+  }
+};
 
-module.exports= {describeVPCandInstances}
+const getInstances = async () => {
+  try {
+    const response = await ec2Client.send(new DescribeInstancesCommand({}));
+    console.log(typeof response, JSON.stringify(response));
+    const instances = response.Reservations.Instances;
+    const data = instances.map((instance) => {
+      const instanceName = instance.Tags?.[0].Value || "-";
+      return {
+        name: instanceName,
+        id: instance.InstanceId,
+        state: instance.State.Name,
+        keyName: instance.KeyName,
+      };
+    });
+
+    return { data };
+  } catch (err) {
+    console.log("Oh no!!!.. Error from instances %j");
+    return { data: [], error: err.message };
+  }
+};
+
+const describeVPCandInstances = async (req, res) => {
+  const vpc = await getVPCs();
+  const instances = await getInstances();
+  //console.log(instances);
+  //res.send("hello fm vpc");
+  res.render("index.ejs", { vpc, instances });
+};
+
+module.exports = { describeVPCandInstances };
