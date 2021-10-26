07b4ac3299f872896dcf324822079af7c878268b
siddhant209 <siddhant_jaiswal@applicatetechnology.com>
2021-10-26 17:23:56 +0530

    Docs font and createEnv response

--- controllers/createEnvironment.js
+++ controllers/createEnvironment.js
@@ -9,23 +9,27 @@ const createNewEnvironment = async (req, res) => {
   const { stackName, vpcName, vpcCIDR, publicSubnetCIDR, privateSubnetCIDR } =
     req.body;
   const body = {};
-  if (!cirdValidation(vpcCIDR, publicSubnetCIDR, privateSubnetCIDR)) {
+  /* if (!cirdValidation(vpcCIDR, publicSubnetCIDR, privateSubnetCIDR)) {
     console.log("invalid");
     return res.json({ errorMessage: "Incorrect CIDR IPs" });
   } else {
     console.log("valid!!");
     return res.json({ errorMessage: "Valid inputs" });
-  }
+  } */
   try {
-    const response = await axios.post(
+    const { data } = await axios.post(
       "https://26rwihrqol.execute-api.us-east-1.amazonaws.com/dev/createenvironment",
       { stackName, vpcName, vpcCIDR, publicSubnetCIDR, privateSubnetCIDR }
     );
-    console.log(response);
-    return res.json(response.data);
+    console.log(data);
+    if (data.success === "true")
+      return res.status(200).json({ data: data.data });
+    else return res.status(400).json({ errorMessage: data.message });
   } catch (e) {
-    console.log("oh no", e);
-    return res.json({ statusCode: 400, body: e });
+    console.log(e);
+    return res
+      .status(400)
+      .json({ errorMessage: "Error: " + e.response.data.message });
   }
 };
 module.exports = { getCreateEnvironmentForm, createNewEnvironment };
