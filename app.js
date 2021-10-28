31c7241abcfcefe8dd2d9c34291c8f54266a64bf
siddhant209 <siddhant_jaiswal@applicatetechnology.com>
2021-10-28 15:43:05 +0530

    added login

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const path = require("path");
 const fs = require("fs");
 const describeVPCandInstances = require("./routes/describeVPCandInstances");
 const startStopInstance = require("./routes/startStopInstance");
@@ -11,24 +15,68 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/views"));
 app.use(express.json());
-// Import the Amazon S3 service client
-/* const S3 = require("@aws-sdk/client-s3"); 
-// Create an S3 client in the us-west-1 Region
-const s3Client = new S3.S3Client({
-    region: "us-west-1"
-  });
+// cookie parser middleware
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-console.log(s3Client) */
+// creating 24 hours from milliseconds
+const oneDay = 1000 * 60 * 60 * 24;
+//username and password
+const myusername = "admin";
+const mypassword = "admin";
+
+// a variable to save a session
+//session middleware
+app.use(
+  session({
+    secret: "Keep it secret",
+    name: "uniqueSessionID",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.get("/login", (req, res) => {
+  res.sendFile("login.html", { root: path.join(__dirname, "views") });
+});
+const check = (req, res, next) => {
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+};
 
 app.use("/", describeVPCandInstances);
 app.use("/startStopInstance", startStopInstance);
 app.use("/createInstance", createInstance);
 app.use("/createEnvironment", createEnvironment);
-app.use("/contact", contact);
+app.use("/contact", check, contact);
 app.use("/terminateInstance", terminateInstance);
 app.use("/docs", docs);
-app.get("/login", (req, res) => {
-  res.render("login.ejs");
+
+app.post(
+  "/authenticate",
+
+  (req, res, next) => {
+    console.log(req.body);
+    // Actual implementation would check values in a database
+    if (req.body.username == "foo" && req.body.password == "bar") {
+      res.locals.username = req.body.username;
+      console.log("valid!");
+      next();
+    } else res.sendStatus(401);
+  },
+  (req, res) => {
+    req.session.loggedIn = true;
+    req.session.username = res.locals.username;
+    console.log(req.session);
+    res.redirect("/");
+  }
+);
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {});
+  res.redirect("/login");
+  res.send("Thank you! Visit again");
 });
 app.listen(3000, () => {
   console.log("listening on port 3000...");
